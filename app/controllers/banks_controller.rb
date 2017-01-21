45aee6046cea71461bf556a7c07ebb7ef3742a8b
Legitcoder <U_Moin75@yahoo.com>
2017-01-20 20:40:22 -0500

    Clean Banks Controller Indentation

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -1,13 +1,11 @@
 class BanksController < ApplicationController
 
 	def update
-		
 		bank = Bank.find(params[:id])
 		bank.balance = bank.balance + params[:value].to_i
 		if bank.save
 			bank.child.undeposited_funds = bank.child.undeposited_funds - params[:value].to_i
 			bank.child.save
-			
 			# bank.child.banks.order('type')
 			current_user = child_user.as_json(methods: :dollars, include: [{banks: {methods: [:type, :dollars, :accumulated_interest, :interest_dollars]}}, rewards: {methods: :dollars}])
 			render json: current_user
@@ -16,15 +14,11 @@ class BanksController < ApplicationController
 
 	def transfer
 		child = current_user.children.find_by(name: params[:child])
-
 		if params[:amount].include? "."
 			amount = float_to_whole(params[:amount])
 		else
 			amount = (params[:amount].to_i * 100)
 		end
-
-
-
 		bank = child.banks.find_by(type: params[:fromBank])
 		if (bank.balance-amount) < 0
 			@error = "#{bank.type} does not have enough funds"
@@ -41,7 +35,6 @@ class BanksController < ApplicationController
 	end
 
 	 def withdraw
-
 	 	if withdraw_params[:amount].include? "."
 			amount = float_to_whole(withdraw_params[:amount])
 		else
@@ -54,7 +47,7 @@ class BanksController < ApplicationController
     new_amount = @bank.balance -= amount
     if new_amount < 0
     	@error = "#{withdraw_params['banktype']} does not have enough funds"
-    		@children = @child.parent.children
+    	@children = @child.parent.children
     	render json: {children: @children.as_json(methods: [:total_balance, :dollars], include:{ banks:{methods: [:type, :dollars]} }), error: @error.as_json }
     else
     @bank.update_attribute("balance", new_amount)
